fc83d9968313c9878b7428a2254f1212d689213e
gideonadeti <gideonadeti0@gmail.com>
2024-10-15 17:47:41 +0000

    Add sidebar navs

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,10 +1,46 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+const sidebarNavs = [
+  { name: "All", filter: "all" },
+  { name: "Published", filter: "published" },
+  { name: "Unpublished", filter: "unpublished" },
+];
+
 export default function Sidebar() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleNavClick(filter: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (filter) {
+      params.set("filter", filter);
+    } else {
+      params.delete("filter");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div
       className="container-fluid position-absolute top-0 bottom-0 border-end"
       style={{ width: "280px" }}
     >
-      <p>Sidebar</p>
+      <div className="d-flex flex-column gap-2 mt-3">
+        {sidebarNavs.map((nav) => (
+          <div
+            key={nav.name}
+            className="btn text-decoration-none bg-light px-2 py-1 rounded"
+            onClick={() => handleNavClick(nav.filter)}
+          >
+            {nav.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
