ae9efb2f493c3f3812020d96b969572180420dd1
gideonadeti <gideonadeti0@gmail.com>
2024-10-16 19:53:01 +0000

    Prevent creation of posts if there're no tags

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,12 +3,13 @@
 import { useState } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
+import { Tag } from "@prisma/client";
 
 import { usePostsStore } from "../stores/posts";
+import { useTagsStore } from "../stores/tags";
 
 const sidebarNavs = [
   { name: "All", filter: "all" },
@@ -67,6 +68,7 @@ export default function Sidebar() {
 interface CreateForm {
   title: string;
   content: string;
+  tags: Tag[];
 }
 
 function CreatePostModal({
@@ -78,7 +80,6 @@ function CreatePostModal({
 }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
   const {
     register,
     handleSubmit,
@@ -86,6 +87,7 @@ function CreatePostModal({
     reset,
   } = useForm<CreateForm>();
   const { setPost } = usePostsStore();
+  const { tags } = useTagsStore();
 
   async function onSubmit(formData: CreateForm) {
     try {
@@ -121,53 +123,75 @@ function CreatePostModal({
       </Modal.Header>
       <Modal.Body>
         {error && <p className="text-danger text-center">{error}</p>}
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Form.Group className="mb-3">
-            <Form.Label>Title</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter title"
-              {...register("title", { required: "Title is required" })}
-              autoFocus
-            />
-            {errors.title && (
-              <Form.Text className="text-danger">
-                {errors.title.message}
-              </Form.Text>
-            )}
-          </Form.Group>
+        {tags.length > 0 ? (
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter title"
+                {...register("title", { required: "Title is required" })}
+                autoFocus
+              />
+              {errors.title && (
+                <Form.Text className="text-danger">
+                  {errors.title.message}
+                </Form.Text>
+              )}
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Label>Content</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={12}
-              {...register("content", { required: "Content is required" })}
-            />
-            {errors.content && (
-              <Form.Text className="text-danger">
-                {errors.content.message}
-              </Form.Text>
-            )}
-          </Form.Group>
-          <Form.Group className="mb-3 d-flex justify-content-end gap-3">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={handleClose}
-              disabled={loading}
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="btn btn-primary"
-              disabled={loading}
-            >
-              {loading ? "Creating..." : "Create"}
-            </button>
-          </Form.Group>
-        </Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Content</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={12}
+                {...register("content", { required: "Content is required" })}
+              />
+              {errors.content && (
+                <Form.Text className="text-danger">
+                  {errors.content.message}
+                </Form.Text>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Tags</Form.Label>
+              <Form.Select
+                multiple
+                {...register("tags", { required: "Tags are required" })}
+              >
+                {tags.map((tag) => (
+                  <option key={tag.id} value={tag.id}>
+                    {tag.name}
+                  </option>
+                ))}
+              </Form.Select>
+              {errors.tags && (
+                <Form.Text className="text-danger">
+                  {errors.tags.message}
+                </Form.Text>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3 d-flex justify-content-end gap-3">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleClose}
+                disabled={loading}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={loading}
+              >
+                {loading ? "Creating..." : "Create"}
+              </button>
+            </Form.Group>
+          </Form>
+        ) : (
+          <p className="text-center">Please create a tag first</p>
+        )}
       </Modal.Body>
     </Modal>
   );
