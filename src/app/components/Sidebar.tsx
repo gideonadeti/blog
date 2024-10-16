98793af5241de4ea829d15c8e06baa48c5e6db5d
gideonadeti <gideonadeti0@gmail.com>
2024-10-16 06:17:21 +0000

    Add form validation to create post form

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 
@@ -59,6 +61,11 @@ export default function Sidebar() {
   );
 }
 
+interface CreateForm {
+  title: string;
+  content: string;
+}
+
 function CreatePostModal({
   open,
   setOpen,
@@ -66,10 +73,26 @@ function CreatePostModal({
   open: boolean;
   setOpen: (open: boolean) => void;
 }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<CreateForm>();
+
+  async function onSubmit(data: CreateForm) {
+    console.log(data);
+  }
+
+  function handleClose() {
+    reset();
+    setOpen(false);
+  }
+
   return (
     <Modal
       show={open}
-      onHide={() => setOpen(!open)}
+      onHide={handleClose}
       backdrop="static"
       keyboard={false}
       fullscreen
@@ -78,30 +101,48 @@ function CreatePostModal({
         <Modal.Title>Create Post</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group className="mb-3">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Enter title" />
+            <Form.Control
+              type="text"
+              placeholder="Enter title"
+              {...register("title", { required: "Title is required" })}
+            />
+            {errors.title && (
+              <Form.Text className="text-danger">
+                {errors.title.message}
+              </Form.Text>
+            )}
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Content</Form.Label>
-            <Form.Control as="textarea" rows={15} />
+            <Form.Control
+              as="textarea"
+              rows={12}
+              {...register("content", { required: "Content is required" })}
+            />
+            {errors.content && (
+              <Form.Text className="text-danger">
+                {errors.content.message}
+              </Form.Text>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3 d-flex justify-content-end gap-3">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleClose}
+            >
+              Cancel
+            </button>
+            <button type="submit" className="btn btn-primary">
+              Create
+            </button>
           </Form.Group>
         </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={() => setOpen(!open)}
-        >
-          Cancel
-        </button>
-        <button type="button" className="btn btn-primary">
-          Create
-        </button>
-      </Modal.Footer>
     </Modal>
   );
 }
