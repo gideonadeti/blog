3c0e84220dee8114ed1deced372a80a42495c385
gideonadeti <gideonadeti0@gmail.com>
2024-10-16 05:04:32 +0000

    Set up modal for creating posts

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useState } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 const sidebarNavs = [
   { name: "All", filter: "all" },
@@ -9,6 +12,8 @@ const sidebarNavs = [
 ];
 
 export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -27,10 +32,10 @@ export default function Sidebar() {
 
   return (
     <div
-      className="container-fluid position-absolute top-0 bottom-0 border-end"
+      className="container-fluid position-absolute top-0 bottom-0 border-end d-flex flex-column justify-content-between py-3"
       style={{ width: "280px" }}
     >
-      <div className="d-flex flex-column gap-2 mt-3">
+      <div className="d-flex flex-column gap-2">
         {sidebarNavs.map((nav) => (
           <div
             key={nav.name}
@@ -41,6 +46,62 @@ export default function Sidebar() {
           </div>
         ))}
       </div>
+      <div>
+        <div
+          className="btn text-decoration-none bg-light px-2 py-1 rounded w-100"
+          onClick={() => setOpen(!open)}
+        >
+          Create post
+        </div>
+      </div>
+      <CreatePostModal open={open} setOpen={setOpen} />
     </div>
+  );
+}
+
+function CreatePostModal({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  return (
+    <Modal
+      show={open}
+      onHide={() => setOpen(!open)}
+      backdrop="static"
+      keyboard={false}
+      fullscreen
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Create Post</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter title" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Content</Form.Label>
+            <Form.Control as="textarea" rows={15} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={() => setOpen(!open)}
+        >
+          Cancel
+        </button>
+        <button type="button" className="btn btn-primary">
+          Create
+        </button>
+      </Modal.Footer>
+    </Modal>
   );
 }
