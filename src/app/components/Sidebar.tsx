8b784c4d4f2349252a494b0e8004a1fe325bb4b8
gideonadeti <gideonadeti0@gmail.com>
2024-10-17 04:49:29 +0000

    Add some clarity to an abbrv

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -13,7 +13,7 @@ const sidebarNavs = [
 ];
 
 export default function Sidebar() {
-  const [showCPModal, setShowCPModal] = useState(false);
+  const [showCPModal, setShowCPModal] = useState(false); // CP -> CreatePost
   const [showCTModal, setShowCTModal] = useState(false); // CT -> CreateTag
 
   const searchParams = useSearchParams();
