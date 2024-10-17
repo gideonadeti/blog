372b0ab269489d89da00376fb9fa3772883cfae0
gideonadeti <gideonadeti0@gmail.com>
2024-10-17 14:07:43 +0000

    Wrap Sidebar and Content in Suspense

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,11 +1,23 @@
+import { Suspense } from "react";
+
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 
 export default function Main() {
   return (
     <main className="flex-grow-1 position-relative">
-      <Sidebar />
-      <Content />
+      <Suspense
+        fallback={
+          <div className="d-flex justify-content-center align-items-center">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        }
+      >
+        <Sidebar />
+        <Content />
+      </Suspense>
     </main>
   );
 }
